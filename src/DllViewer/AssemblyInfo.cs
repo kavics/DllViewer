caade187d9edd867739bf49e7fdec6e4a6995e8d
Zoltan Gyebrovszki <gyeby5@gmail.com>
2018-04-01 02:55:23 +0200

    Assenbly card.

--- src/DllViewer/AssemblyInfo.cs
+++ src/DllViewer/AssemblyInfo.cs
@@ -27,17 +27,23 @@ namespace DllViewer
         public DateTime LastAccessTime => _fileInfo.LastAccessTime;
 
         public Version Version => _assemblyName.Version;
-        public string FrameworkVersion => _assembly.ImageRuntimeVersion;
+        public string ImageRuntimeVersion => _assembly.ImageRuntimeVersion;
 
         public string Title { get; }
         public string Description { get; }
         public string Company { get; }
         public string Guid { get; }
         public string TargetFramework { get; }
+        public string FrameworkVersion { get; }
         public string Copyright { get; }
         public string Product { get; }
         public string Trademark { get; }
         public string DebugInfo { get; }
+
+        public AssemblyName[] References { get; }
+        public string ReferencesAsText { get; }
+
+        public string NameAndVersion => $"{Name}.{Kind.ToString().ToLower()}   v{Version} {(FrameworkVersion == null ? "" : "  fw:" + FrameworkVersion)}";
 
         public AssemblyInfo(string path)
         {
@@ -70,10 +76,17 @@ namespace DllViewer
                     case "System.Reflection.AssemblyDescriptionAttribute": Description = attr.ConstructorArguments[0].Value.ToString(); break;
                     case "System.Reflection.AssemblyCompanyAttribute": Company = attr.ConstructorArguments[0].Value.ToString(); break;
                     case "System.Runtime.InteropServices.GuidAttribute": Guid = attr.ConstructorArguments[0].Value.ToString(); break;
-                    case "System.Runtime.Versioning.TargetFrameworkAttribute": TargetFramework = attr.ConstructorArguments[0].Value.ToString(); break;
                     case "System.Reflection.AssemblyCopyrightAttribute": Copyright = attr.ConstructorArguments[0].Value.ToString(); break;
+                    case "System.Runtime.Versioning.TargetFrameworkAttribute":
+                        TargetFramework = attr.ConstructorArguments[0].Value.ToString();
+                        if (TargetFramework.StartsWith(".NETFramework,Version=v"))
+                            FrameworkVersion = TargetFramework.Replace(".NETFramework,Version=v", "");
+                        break;
                 }
             }
+
+            References = _assembly.GetReferencedAssemblies();
+            ReferencesAsText = string.Join(Environment.NewLine, References.Select(r => r.Name).ToArray());
         }
 
         private string GetDebugInfo(object value)
